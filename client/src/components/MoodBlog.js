15260f24157458e70658d0597881689d022d8091
Kevin Haiduy Phung <kphung29@gmail.com>
2019-03-25 19:19:01 -0400

    created moodblog component and updated styling

---
+++ client/src/components/MoodBlog.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Grid, Image, Header, HeaderContent } from 'semantic-ui-react'
+
+const MoodBlog = () => (
+  <Grid celled>
+  <Grid.Row>
+    <Grid.Column width={3}>
+      <Image src='https://cdn.shopify.com/s/files/1/1728/2157/articles/4.jpg?v=1552669333' />
+    </Grid.Column>
+    <Grid.Column width={13}>
+      <Header as='h2'>
+      <HeaderContent>
+      <p>
+      <p textAlign='center' style={{paddingTop: 10}}>MINDFULNESS MEDITATION <h4>posted on 3/25/19</h4></p>
+Mindfulness meditation encourages the practitioner to observe wandering thoughts as they drift through the mind. The intention is not to get involved with the thoughts or to judge them, but simply to be aware of each mental note as it arises.
+Through mindfulness meditation, you can see how your thoughts and feelings tend to move in particular patterns. Over time, you can become more aware of the human tendency to quickly judge an experience as good or bad, pleasant or unpleasant. With practice, an inner balance develops.
+In some schools of meditation, students practice a combination of concentration and mindfulness. Many disciplines call for stillness — to a greater or lesser degree, depending on the teacher.
+      <p textAlign='center' style={{paddingTop: 10}}>OTHER MEDITATION TECHNIQUES</p>
+There are various other meditation techniques. For example, a daily meditation practice among Buddhist monks focuses directly on the cultivation of compassion. This involves envisioning negative events and recasting them in a positive light by transforming them through compassion.
+There are also moving meditation techniques, such as tai chi, qigong, and walking meditation.
+      <p textAlign='center' style={{paddingTop: 10}}>BENEFITS OF MEDITATION</p>
+If relaxation is not the goal of meditation, it is often a result. In the 1970s, Herbert Benson, MD, a researcher at Harvard University Medical School, coined the term “relaxation response" after conducting research on people who practiced transcendental meditation. The relaxation response, in Benson’s words, is “an opposite, involuntary response that causes a reduction in the activity of the sympathetic nervous system.”
+Since then, studies on the relaxation response have documented the following short-term benefits to the nervous system:
+Lower blood pressure
+Improved blood circulation
+Lower heart rate
+Less perspiration
+Slower respiratory rate
+Less anxiety
+Lower blood cortisol levels
+More feelings of well-being
+Less stress
+Deeper relaxation
+Contemporary researchers are now exploring whether a consistent meditation practice yields long-term benefits, and noting positive effects on brain and immune function among meditators. Yet it’s worth repeating that the purpose of meditation is not to achieve benefits. To put it as an Eastern philosopher may say, the goal of meditation is no goal. It’s simply to be present.
+In Buddhist philosophy, the ultimate benefit of meditation is liberation of the mind from attachment to things it cannot control, such as external circumstances or strong internal emotions. The liberated or “enlightened” practitioner no longer needlessly follows desires or clings to experiences, but instead maintains a calm mind and sense of inner harmony.
+      <br />
+      <br />
+      <p textAlign='center' style={{paddingTop: 10}}>HOW TO MEDITATE: SIMPLE MEDITATION FOR BEGINNERS</p>
+This meditation exercise is an excellent introduction to meditation techniques.
+Sit or lie comfortably. You may even want to invest in a meditation chair or cushion.
+Close your eyes. We recommend using one of our Cooling Eye Masks or Restorative Eye Pillows if lying down.
+Make no effort to control the breath; simply breathe naturally.
+Focus your attention on the breath and on how the body moves with each inhalation and exhalation. Notice the movement of your body as you breathe. Observe your chest, shoulders, rib cage, and belly. Simply focus your attention on your breath without controlling its pace or intensity. If your mind wanders, return your focus back to your breath.
+Maintain this meditation practice for two to three minutes to start, and then try it for longer periods.
+</p>
+<p style={{paddingBottom: 20}}></p>
+      </HeaderContent>
+      </Header>
+    </Grid.Column>
+  </Grid.Row>
+  <Grid.Row>
+    <Grid.Column width={3}>
+      <Image src='https://images.agoramedia.com/everydayhealth/gcms/Amazing-Benefits-of-Exercise-01-722x406.jpg' />
+    </Grid.Column>
+    <Grid.Column width={13}>
+      <Header as='h2'>
+      <HeaderContent>
+      <p>
+      <p textAlign='center' style={{paddingTop: 10}}>EXERCISE BOOSTS PHYSICAL, MENTAL, AND EMOTIONAL HEALTH <h4>posted on 3/23/19</h4></p>
+      < br />
+      If you want to lose weight and keep it off, you know that exercise should be an essential part of your routine. But the benefits of physical activity go far beyond just physical fitness. Increasingly, more and more research is showing that working out regularly can boost other aspects of your health as well, including cognitive function and emotional well-being.
+A regular fitness routine has been shown to have a wide-range of positive health effects, such as a lower risk of cancer and stroke, better cardiovascular health, stronger muscles, and slowing of bone density loss associated with age.
+Exercise has also been linked to better brain health and emotional well-being. And a study published in the journal Circulation found that exercise, even without weight loss, may help you live longer.
+Here’s how Christopher Berger, PhD, CSCS, an exercise physiologist and clinical assistant professor at Arizona State University in Pheonix, sums it up: “Everything improves with exercise.”
+And perhaps one of the best new findings about exercise — especially if you, like many people, struggle to find the time to fit it into a busy day — is that all those benefits of physical activity can be had even if you only squeeze in a few minutes of exercise a day. While doctors used to think that we needed to engage in 30 to 60 minutes of exercise a day, new research is finding that we can see benefits with shorter bursts of physical activity. “As little as 15 minutes a day of high-intensity activity that leaves you breathless, like swimming, can kick start your metabolic rate and reduce body fat and increase muscle mass,” says Dr. Berger.
+In a widely-cited study published in February 2013 in the American Journal of Health Promotion, researchers at Oregon State University looked at more than 6,000 American adults and found that even small amounts of physical activity — like pacing while talking on the phone or doing some jumping jacks during commercials while watching TV, as long as these short bursts of exercise add up to 30 minutes a day — can be just as beneficial as longer workout sessions at the gym.
+And you don’t need to sweat buckets to see the benefits of exercise, either. According to the physical activity guidelines from the Centers for Disease Control and Prevention (CDC), 150 minutes of moderate-intensity exercise with two days of strength training per week yields the same health benefits as 75 minutes of high-intensity exercise with two days of strength training each week — as does a combination of moderate- and high-intensity exercise, plus two strength training workouts.
+So what are some of the many ways exercise can benefit your health? Read on to learn about 10 health reasons to break a sweat today.
+</p>
+<p style={{paddingBottom: 20}}></p>
+      </HeaderContent>
+      </Header>
+    </Grid.Column>
+  </Grid.Row>
+  <Grid.Row>
+    <Grid.Column width={3}>
+      <Image src='https://media.self.com/photos/586540bc733664ae3e5ee9f8/4:3/w_752,c_limit/eat-healthy_feature.png' />
+    </Grid.Column>
+    <Grid.Column width={13}>
+      <Header as='h2'>
+      <HeaderContent>
+      <p>
+      <p textAlign='center' style={{paddingTop: 10}}>WHAT IS A HEALTHY EATING STYLE? <h4>posted on 3/21/19</h4></p>
+      There is more than one way to eat healthfully and everyone has their own eating style. Make healthier choices that reflect your preferences, culture, traditions, and budget. Choose fruits, vegetables, grains, dairy, and protein foods to get the most nutrition and meet your personal calorie needs. Aim for a variety of foods and beverages from each food group and limit saturated fat, sodium, and added sugars.
+
+      <p textAlign='center' style={{ paddingTop: 20 }}>Everything You Eat and Drink Matters — Focus on Variety, Amount, and Nutrition</p>
+
+What and how much you eat and drink, along with regular physical activity, can help you manage your weight and lower your risk of disease.
+
+<p textAlign='center' style={{paddingTop: 10}}>Choose Foods and Beverages with Less Saturated Fat, Sodium, and Added Sugars</p>
+
+The saturated fat, sodium, and added sugars found in foods and beverages are important for you to think about as you build your healthy eating style. Saturated fat and sodium are sometimes found naturally in foods and beverages. Sugars, sodium, and ingredients high in saturated fat can also be added during processing or preparing foods and beverages.
+
+<p textAlign='center' style={{paddingTop: 30, paddingBottom: 10}}>Start with Small Changes</p>
+Create an eating style that can improve your health now and in the future by making small changes over time. Consider changes that reflect your personal preferences, culture and traditions. Think of each change as a “win” as you build positive habits and find solutions that reflect your healthy eating style. Each change is a MyWin that can help you build your healthy eating style. Use the tips and links below to find little victories that work for you.
+</p>
+<p style={{paddingBottom: 20}}></p>
+      </HeaderContent>
+      </Header>
+    </Grid.Column>
+  </Grid.Row>
+</Grid>
+)
+
+
+export default MoodBlog;
