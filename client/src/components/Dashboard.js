74dc6d6cfdf825fbfa893e9c593a2b8bf59e5cd9
Kevin Haiduy Phung <kphung29@gmail.com>
2019-03-26 17:30:19 -0400

    fixed any broken links and refractored dashboard component

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,48 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Grid, GridColumn, GridRow } from 'semantic-ui-react';
 
 import MoodModal from './MoodModal';
 import Chart from './MoodChart';
 import MoodCard from './MoodCard';
 
-class Dashboard extends Component {
-  constructor(props) {
-    super(props);
+const Dashboard = () => (
 
-    this.state = {
-      users: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('/posts')
-    .then(res => res.json())
-    .then(data => this.setState({
-      users: data,
-    }))
-    .catch(err => console.log(err));
-  }
-
-  render() {
-    const { users } = this.state;
-    return (
-      <div>
-      <Grid style={{ paddingLeft: 20 }} columns={2} celled>
-        <GridRow>
-          <GridColumn width={3} textAlign='center'>
-            <MoodCard users={users}/>
-            <br/>
-            <br/>
-            <MoodModal />
-          </GridColumn>
-          <GridColumn width={13}>
-            <Chart />
-          </GridColumn>
-        </GridRow>
-      </Grid>
-      </div>
-    )
-  }
-}
+  <div>
+  <Grid style={{ paddingLeft: 20 }} columns={2} celled>
+    <GridRow>
+      <GridColumn width={3} textAlign='center'>
+        <MoodCard />
+        <br/>
+        <br/>
+        <MoodModal />
+      </GridColumn>
+      <GridColumn width={13}>
+        <Chart />
+      </GridColumn>
+    </GridRow>
+  </Grid>
+  </div>
+)
 
 export default Dashboard;
--- client/src/components/MoodCard.js
+++ client/src/components/MoodCard.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Card, Icon, Image, Grid } from 'semantic-ui-react'
+import { withRouter } from 'react-router-dom';
 
-const MoodCard = (props) => (
-  <Grid textAlign='center'>
-  <Card>
-    <Image src='https://s3.amazonaws.com/moodappmvp/profile.jpg' />
-    <Card.Content>
-      <Card.Header>Kevin</Card.Header>
-      <Card.Meta>Joined in 2019</Card.Meta>
-      <Card.Description>Kevin is a medical professional/software engineer in Boston.</Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='book' />
-        {props.users.length} Total Mood Entries
-      </a>
-    </Card.Content>
-  </Card>
-  </Grid>
-)
+class MoodCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
 
-export default MoodCard;
+    this.routeChange = this.routeChange.bind(this);
+  }
+
+  routeChange() {
+    let path = `history`;
+    this.props.history.push(path);
+  }
+
+  componentDidMount() {
+    fetch('/posts')
+    .then(res => res.json())
+    .then(data => this.setState({
+      users: data,
+    }))
+    .catch(err => console.log(err));
+  }
+
+  render() {
+    const { users } = this.state;
+    return (
+      <Grid textAlign='center'>
+      <Card>
+        <Image src='https://s3.amazonaws.com/moodappmvp/profile.jpg' />
+        <Card.Content>
+          <Card.Header>Kevin</Card.Header>
+          <Card.Meta>Joined in 2019</Card.Meta>
+          <Card.Description>Kevin is a medical professional/software engineer in Boston.</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <a onClick={this.routeChange}>
+            <Icon name='book' />
+            {users.length} Total Mood Entries
+          </a>
+        </Card.Content>
+      </Card>
+      </Grid>
+    )
+  }
+}
+
+export default withRouter(MoodCard);
